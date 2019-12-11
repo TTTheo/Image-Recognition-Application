722e1173ff9d3d717c42b5a38a1d3cb3958f653f
Peilun Dai <pdai@dhcp-wifi-8021x-155-41-50-140.bu.edu>
2019-12-11 12:40:17 -0500

    updated setup

--- NNServer/server_setup.sh
+++ NNServer/server_setup.sh
@@ -4,7 +4,13 @@ sudo apt-get update
 
 
 # install java
-
+sudo mkdir java
+cd java
+sudo wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
+sudo tar -xf openjdk-13.0.1_linux-x64_bin.tar.gz
+cd ..
+export JAVA_HOME=./java/jdk-13.0.1
+export PATH=$JAVA_HOME/bin:$PATH
 
 # install Python
 sudo apt-get -y install python-pip
