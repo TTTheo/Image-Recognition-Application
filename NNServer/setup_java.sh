f231477f14a39160ec0ff4b9b0ec79a1e7a63095
ljensen3 <37462140+ljensen3@users.noreply.github.com>
2019-12-11 13:51:04 -0500

    Update setup_java.sh

--- NNServer/setup_java.sh
+++ NNServer/setup_java.sh
@@ -12,3 +12,18 @@ echo 'export JAVA_HOME=~/java/jdk-13.0.1' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc 
 source ~/.bashrc
 
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+#wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+#bash Anaconda3-4.0.0-Linux-x86_64.sh -b
+export PATH=~/miniconda3/bin:$PATH
+conda update -y conda
+
+echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+conda install -y pytorch torchvision cpuonly -c pytorch
+
+
