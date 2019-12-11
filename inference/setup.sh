64854aa4966d37507599cebe0f2bcbabd4e8fe63
ljjensen <ljjensen@pc2.fin.ch-geni-net.geni.kettering.edu>
2019-12-11 09:08:40 -0500

    setup

---
+++ inference/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get -y install python-pip
+sudo pip2 install numpy
+wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+bash Anaconda3-4.0.0-Linux-x86_64.sh -b
+export PATH=/users/ljjensen/anaconda3/bin:$PATH
+
+conda update -y conda
+
+conda install -y pytorch torchvision cpuonly -c pytorch
+
