322c62d0c6f3919258f782c91f1709b6b9865d3c
Peilun Dai <pdai@dhcp-wifi-8021x-155-41-50-140.bu.edu>
2019-12-11 12:37:57 -0500

    merged server

--- NNServer/server_setup.sh
+++ NNServer/server_setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-
 sudo apt-get update
+
+
+# install java
+
+
+# install Python
 sudo apt-get -y install python-pip
 sudo pip2 install numpy
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
 bash Anaconda3-4.0.0-Linux-x86_64.sh -b
-export PATH=/users/ljjensen/anaconda3/bin:$PATH
-
+export PATH=~/anaconda3/bin:$PATH
 conda update -y conda
-
 conda install -y pytorch torchvision cpuonly -c pytorch
 
